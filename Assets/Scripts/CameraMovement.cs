72ad6c80581b503160dd19bc48a6093e1ded8596
Sjoeland <77164355+Sjoeland@users.noreply.github.com>
2022-04-02 01:19:15 +0200

    Player Movement and Interaction

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraMovement : MonoBehaviour
+{
+
+    private float rotationSpeed = 500f;
+    private GameObject player;
+    private float interactRange = 2f;
+    private bool isInteracting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        player = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float r = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime * -1f;
+        r += transform.localEulerAngles.x;
+
+        transform.localEulerAngles = new Vector3(r, 0f, 0f);
+
+        r = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+        r += player.transform.localEulerAngles.y;
+
+        player.transform.localEulerAngles = new Vector3(0f, r, 0f);
+
+        if (!Input.GetButtonDown("Fire1")) { return; }
+        // Only execute the following code if the button is pressed
+        isInteracting = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isInteracting) { return; }
+        // Only execute the following code if the button is pressed
+        RaycastHit hit;
+        Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, interactRange))
+        {
+            // For testing only
+            hit.transform.gameObject.SetActive(false);
+        }
+        isInteracting = false;
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float speed = 5f;
+    private CharacterController character;
+
+    private void Awake()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        Vector3 movement = transform.forward * y + transform.right * x;
+
+        character.Move(movement);
+    }
+
+}
